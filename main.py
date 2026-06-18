d6a52d5963b4692fbc25381e59b75d210da6ca1e
aleyakovlev-beep <ale.yakovlev@unact.ru>
2026-06-18 15:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, Request, Form
+from fastapi import FastAPI, Depends, Request, Form, Response
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
